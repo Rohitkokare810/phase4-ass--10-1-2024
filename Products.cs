8d4be9040a9e94ecacdd9665702e991ce57267e8
RohitKokare810 <Rohitkokare810@gmail.com>
2024-01-10 15:55:58 +0530

    ass2

---
+++ Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Products
+{
+    public string PName { get; set; }
+    public int PPrice { get; set; }
+
+    public string Brand { get; set; }
+    public DateTime MDate { get; set; }
+    public DateTime EDate { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<Products> products = new List<Products>()
+{
+    new Products() { PName="Mobile",PPrice=22000,Brand="Samsung",MDate=new DateTime(day:12,month:11,year:2022),EDate=new DateTime(day:14,month:12,year:2024)},
+   new Products() { PName="TV",PPrice=22000,Brand="Samsung",MDate=new DateTime(day:12,month:11,year:2022),EDate=new DateTime(day:14,month:12,year:2024)},
+   new Products() { PName="Smart Watch",PPrice=22000,Brand="Samsung",MDate=new DateTime(day:12,month:11,year:2022),EDate=new DateTime(day:14,month:12,year:2024)},
+   new Products() { PName="Mobile",PPrice=22000,Brand="Samsung",MDate=new DateTime(day:12,month:11,year:2022),EDate=new DateTime(day:14,month:12,year:2024)},
+   new Products() { PName="Headphone",PPrice=22000,Brand="Samsung",MDate=new DateTime(day:12,month:11,year:2022),EDate=new DateTime(day:14,month:12,year:2024)},
+};
+Console.WriteLine("PName \t PPrice \t Brand \t MDate \t\t EDate");
+foreach (Products pro in products)
+{
+    Console.Write(pro.PName + "\t \t");
+    Console.Write(pro.PPrice + "\t \t");
+    Console.Write(pro.Brand + "\t \t");
+    Console.Write(pro.MDate.ToLongDateString() + "\t \t");
+    Console.Write(pro.EDate.ToLongDateString());
+
+    Console.WriteLine("\n");
+}
+
